f513f8f24efb395ee39e640dd6a1af7ae9094e99
MadhaviKandala <madhavidonthi@gmail.com>
2023-12-06 17:25:06 +0530

    feature2

--- TestProject.java
+++ TestProject.java
@@ -1,5 +1,5 @@
 public class TestProject {
     public static void main(String[] args) {
-        System.out.println("git   feature branch 2");
+        System.out.println("git feature branch 2");
     }
 }
