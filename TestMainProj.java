fd27e88d31b0c4c9b8b9095f8332864c7b7502c5
MadhaviKandala <madhavidonthi@gmail.com>
2023-12-07 10:00:11 +0530

    main commit

---
+++ TestMainProj.java
@@ -0,0 +1,8 @@
+public class TestMainProj {
+    System.out.println("Line2");
+    System.out.println("Line4");
+    System.out.println("Line6");
+    System.out.println("Line8");
+    System.out.println("Line10");
+    System.out.println("Line12");
+}
