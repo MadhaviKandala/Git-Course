86de354e7caec5c0c92813862858aa4487095d2e
MadhaviKandala <madhavidonthi@gmail.com>
2023-12-07 11:35:58 +0530

    feature2 commit

---
+++ MainProj.java
@@ -0,0 +1,13 @@
+public class MainProj {
+    public static void main(String[] args) {
+
+        System.out.println("Line2");
+        System.out.println("Line4");
+        System.out.println("Line6");
+        System.out.println("Line8");
+        System.out.println("Line10");
+        System.out.println("Line12");
+        System.out.println("Line14");
+
+    }
+}
