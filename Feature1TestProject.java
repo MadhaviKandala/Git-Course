53f1f57a3639c4056b12d96f914f903e42fa5152
MadhaviKandala <madhavidonthi@gmail.com>
2023-12-07 10:13:32 +0530

    main commit

---
+++ Feature1TestProject.java
@@ -0,0 +1,9 @@
+public class Feature1TestProject {
+    public static void main(String[] args) {
+        System.out.println("Line3");
+        System.out.println("Line5");
+        System.out.println("Line7");
+        System.out.println("Line9");
+        System.out.println("Line11");
+    }
+}
--- TestMainProj.java
+++ TestMainProj.java
@@ -1,8 +1,11 @@
 public class TestMainProj {
-    System.out.println("Line2");
-    System.out.println("Line4");
-    System.out.println("Line6");
-    System.out.println("Line8");
-    System.out.println("Line10");
-    System.out.println("Line12");
+    public static void main(String[] args) {
+
+        System.out.println("Line2");
+        System.out.println("Line4");
+        System.out.println("Line6");
+        System.out.println("Line8");
+        System.out.println("Line10");
+        System.out.println("Line12");
+    }
 }
